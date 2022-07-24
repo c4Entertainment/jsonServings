8c573480859b89db79afcbc17e773e7ae0192abb
Laurent-c4 <laurentjuma.c4@gmail.com>
2022-07-24 07:21:27 +0300

    Rename

--- categoryFinder.js
+++
@@ -1,9 +0,0 @@
-const radio = require("./radio.json");
-const fs = require("fs");
-
-const radioGenresRepeated = [...new Set(radio.radio.map((rad) => rad.genre))];
-const radioGenres = [
-  ...new Set(radioGenresRepeated.toString().replace(/,/g, "/").split("/")),
-].sort();
-const radioGenreJSON = JSON.stringify({ radioGenres: radioGenres });
-fs.writeFileSync("./genre.json", radioGenreJSON);
